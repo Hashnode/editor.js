384cefbcdb1ff777019fb011852194dfec9361e5
Girish Patil <devegpat@gmail.com>
2020-04-16 16:13:46 +0530

    Undo fix

--- src/components/modules/caret.ts
+++ src/components/modules/caret.ts
@@ -432,9 +432,6 @@ export default class Caret extends Module {
     if (force || this.isAtStart) {
       /** If previous Tool`s input exists, focus on it. Otherwise set caret to the previous Block */
       if (!previousInput) {
-        // if (currentBlock.name === 'simpleCode') {
-        //   return false;
-        // }
         this.setToBlock( previousContentfulBlock, this.positions.END );
       } else {
         this.setToInput(previousInput, this.positions.END);
